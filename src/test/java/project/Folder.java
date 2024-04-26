4570d671bc8fb0cc69a68fc81ac6a4911aca0275
janetjoshlina <168186205+janetjoshlina@users.noreply.github.com>
2024-04-26 20:19:56 +0800

    Fix : have made change in sysout

--- src/test/java/project/Folder.java
+++ src/test/java/project/Folder.java
@@ -2,7 +2,7 @@ package project;
 
 public class Folder {
 	public static void main(String[] args) {
-		System.out.println("sample document");
+		System.out.println("sample document change");
 	}
 
 }
